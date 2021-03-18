b1a75ebcd14d8e1d2f39b72fe649faa737f2557a
Mike <mvngr@niuitmo.ru>
2021-03-18 20:47:58 +0300

    Добавил перестановочный шифр

--- transpositioncipher.cpp
+++ transpositioncipher.cpp
@@ -1,8 +1,13 @@
 #include "transpositioncipher.h"
 
-#include <QDebug>
+#include <QtMath>
 
-TranspositionCipher::TranspositionCipher(QObject *parent) : EncryptionAlgorithm(parent)
+TranspositionCipher::TranspositionCipher(QObject *parent) : EncryptionAlgorithm(parent), key_(2)
+{
+
+}
+
+TranspositionCipher::TranspositionCipher(std::size_t key, QObject *parent) : EncryptionAlgorithm(parent), key_(key)
 {
 
 }
@@ -14,20 +19,44 @@ TranspositionCipher::~TranspositionCipher()
 
 QString TranspositionCipher::encrypt(const QString &textForEncrypt)
 {
+    QString encrypted;
+    QString text = textForEncrypt;
 
+    //Добавляем пробелы до прямоугольника
+    if(textForEncrypt.size() % static_cast<int>(key_) > 0)
+        for(int i = 0; i < static_cast<int>(key_) - ( textForEncrypt.size() % static_cast<int>(key_) ); i++)
+        {
+            text += ' ';
+        }
+
+    for(int i = 0; i < static_cast<int>(key_); i++)
+    {
+        for(int j = 0; j < text.size() / static_cast<int>(key_); j++)
+        {
+            encrypted += text.at(j * key_ + i);
+        }
+    }
+    return encrypted;
 }
 
 QString TranspositionCipher::decrypt(const QString &encryptedText)
 {
-
+    return encrypt(encryptedText);
 }
 
 QString TranspositionCipher::defaultKey() const
 {
-    return "qwe";
+    return "2";
 }
 
 bool TranspositionCipher::setKey(const QString &key)
 {
-    return true;
+    bool isOk = false;
+    std::size_t numberKey = key.toUInt(&isOk);
+    if(isOk)
+    {
+        key_ = numberKey;
+    }
+
+    return isOk;
 }
--- transpositioncipher.h
+++ transpositioncipher.h
@@ -8,6 +8,7 @@ class TranspositionCipher : public EncryptionAlgorithm
 
 public:
     explicit TranspositionCipher(QObject *parent = nullptr);
+    explicit TranspositionCipher(std::size_t key, QObject *parent = nullptr);
 
     virtual ~TranspositionCipher();
 
@@ -15,6 +16,9 @@ public:
     QString decrypt(const QString &encryptedText) override;
     QString defaultKey() const override;
     bool setKey(const QString &key) override;
+
+private:
+    std::size_t key_; //количество столбцов
 
 };
 
